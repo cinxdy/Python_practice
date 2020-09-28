1a56c7983b25c8d930e2343c88c55e6c6ad4589f
Cinxdy <cinxdys2@gmail.com>
2020-09-29 02:12:25 +0900

    ???

--- Practical_Project/exercise4.py
+++ Practical_Project/exercise4.py
@@ -5,5 +5,5 @@ a = np.array([7,2,10,2,7,4,9,4,9,8])
 sum_val = 0
 index = np.where( a[])
     if i >= 5 and i <= 10 :
-        sum_val = sum_val + i
+        sum_val = sum_val + i #???
 print(sum_val)
